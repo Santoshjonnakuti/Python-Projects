811e77b7d4342861b78419d92e645a027a950c3e
SantoshJonnakuti <santoshjonnakuti@gmail.com>
2020-12-19 16:19:12 +0530

    Create Main.py

---
+++ Multilingual-Translator/Main.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+import requests
+from bs4 import BeautifulSoup
+
+
+def Function(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    tag = soup.find("div", attrs={"class": "MUxGbd u31kKd gsrt lyLwlc", "id": "lrtl-translation-text"})
+    if tag is None:
+        entry1.insert(0, "Sorry Cannot translate...")
+    else:
+        entry1.insert(0, tag.text)
+    return
+
+
+def urlGenerator():
+    url = "https://www.google.com/search?q="
+    fro = comboBox.get()
+    to = comboBox1.get()
+    value = entry.get()
+    if fro == to:
+        entry1.insert(0, value)
+        return
+    value = value.split()
+    for i in range(len(value)):
+        if i != len(value)-1:
+            url = url + value[i] + "+"
+        else:
+            url = url + value[i]
+    url = url + "+" + fro + "+to+" + to + "+translate"
+    Function(url)
+    return
+
+
+def getLanguages():
+    languagesList = []
+    with open("Languages.txt") as file:
+        for language in file:
+            if "\n" in language:
+                languagesList.append(language[:-1])
+            else:
+                languagesList.append(language)
+    return tuple(languagesList)
+
+
+root = tk.Tk()
+root.title("Multilingual Translator")
+root.iconbitmap("Icon.ico")
+root.geometry("600x500")
+root.resizable(0, 0)
+bgImage = ImageTk.PhotoImage(Image.open("Background.jpg"))
+bgLabel = tk.Label(root, image=bgImage).pack()
+languages = getLanguages()
+fromLabel = tk.Label(root, text="From", bg="black", fg="white", font=("Helvetica", 12, "bold")).place(x=43, y=75, anchor=tk.CENTER)
+comboBox = ttk.Combobox(root, font=("Helvetica", 10))
+comboBox["values"] = languages
+comboBox.place(x=100, y=100, anchor=tk.CENTER)
+entry = tk.Entry(root, width=50, bg="white", fg="black", font=("Helvetica", 10))
+entry.place(x=195, y=125, anchor=tk.CENTER)
+toLabel = tk.Label(root, text="To", bg="black", fg="white", font=("Helvetica", 12, "bold")).place(x=30, y=175, anchor=tk.CENTER)
+comboBox1 = ttk.Combobox(root, font=("Helvetica", 10))
+comboBox1["values"] = languages
+comboBox1.place(x=100, y=200, anchor=tk.CENTER)
+entry1 = tk.Entry(root, width=50, bg="white", fg="black", font=("Helvetica", 10))
+entry1.place(x=195, y=225, anchor=tk.CENTER)
+convertButton = tk.Button(root, text="Convert", bg="black", fg="white", font=("Helvetica", 12, "bold"), command=urlGenerator)
+convertButton.place(x=450, y=175, anchor=tk.CENTER)
+root.mainloop()
