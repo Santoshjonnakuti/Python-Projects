1618b327daeda564ac7d5e535c0bcaa3cf42d9a2
SantoshJonnakuti <santoshjonnakuti@gmail.com>
2020-12-19 09:50:22 +0530

    Update Main.py

--- Currency-Converter/Main.py
+++ Currency-Converter/Main.py
@@ -1,49 +1,33 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+import requests
+from bs4 import BeautifulSoup
 
 
-def convertFunction():
+def Function(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    tags = soup.find("div", attrs={"class": "BNeawe iBp4i AP7Wnd"})
+    tag = tags.find("div", attrs={"class": "BNeawe iBp4i AP7Wnd"})
+    data = tag.text
+    data = data.split()
+    entry1.insert(0, data[0])
+    return 
+
+
+def urlGenerator():
     fro = comboBox.get()
     to = comboBox1.get()
-    val = entry.get()
-    val = float(val)
-    if fro == "Indian Rupee":
-        pass
-    elif fro == "US Dollar":
-        val = val * 73.61
-    elif fro == "British Pound":
-        val = val * 99.62
-    elif fro == "Australian Dollar":
-        val = val * 56.11
-    elif fro == "Canadian Dollar":
-        val = val * 57.55
-    elif fro == "Japanese Yen":
-        val = val * 0.71
-    elif fro == "Swiss Franc":
-        val = val * 83.33
-
-    if to == "Indian Rupee":
-        entry1.delete(0, tk.END)
-        entry1.insert(0, val)
-    elif to == "US Dollar":
-        entry1.delete(0, tk.END)
-        entry1.insert(0,  val * 0.014)
-    elif to == "British Pound":
-        entry1.delete(0, tk.END)
-        entry1.insert(0, val * 0.010)
-    elif to == "Australian Dollar":
-        entry1.delete(0, tk.END)
-        entry1.insert(0, val * 0.018)
-    elif to == "Canadian Dollar":
-        entry1.delete(0, tk.END)
-        entry1.insert(0, val * 0.017)
-    elif to == "Japanese Yen":
-        entry1.delete(0, tk.END)
-        entry1.insert(0, val * 1.40)
-    elif to == "Swiss Franc":
-        entry1.delete(0, tk.END)
-        entry1.insert(0, val * 0.012)
+    value = entry.get()
+    if fro == to:
+        entry1.insert(0, value)
+        return
+    fro = fro.split()
+    to = to.split()
+    url = "https://www.google.com/search?q={}+{}+{}+to+{}+{}".format(value, fro[0], fro[1], to[0], to[1])
+    Function(url)
+    return
 
 
 root = tk.Tk()
@@ -53,19 +37,22 @@ root.geometry("600x500")
 root.resizable(0, 0)
 bgImage = ImageTk.PhotoImage(Image.open("Background.jpg"))
 bgLabel = tk.Label(root, image=bgImage).pack()
+
 fromLabel = tk.Label(root, text="From", bg="black", fg="white", font=("Helvetica", 12, "bold")).place(x=53, y=75, anchor=tk.CENTER)
 comboBox = ttk.Combobox(root, font=("Helvetica", 8))
 comboBox["values"] = ("Indian Rupee", "US Dollar", "British Pound", "Australian Dollar", "Canadian Dollar", "Japanese Yen", "Swiss Franc")
 comboBox.place(x=100, y=100, anchor=tk.CENTER)
 entry = tk.Entry(root, width=15, bg="white", fg="black", font=("Helvetica", 10))
 entry.place(x=83, y=125, anchor=tk.CENTER)
+
 toLabel = tk.Label(root, text="To", bg="black", fg="white", font=("Helvetica", 12, "bold")).place(x=243, y=75, anchor=tk.CENTER)
 comboBox1 = ttk.Combobox(root, font=("Helvetica", 8))
 comboBox1["values"] = ("Indian Rupee", "US Dollar", "British Pound", "Australian Dollar", "Canadian Dollar", "Japanese Yen", "Swiss Franc")
 comboBox1.place(x=300, y=100, anchor=tk.CENTER)
 entry1 = tk.Entry(root, width=15, bg="white", fg="black", font=("Helvetica", 10))
 entry1.place(x=283, y=125, anchor=tk.CENTER)
-convertButton = tk.Button(root, text="Convert", bg="black", fg="white", font=("Helvetica", 12, "bold"), command=convertFunction)
+
+convertButton = tk.Button(root, text="Convert", bg="black", fg="white", font=("Helvetica", 12, "bold"), command=urlGenerator)
 convertButton.place(x=450, y=113, anchor=tk.CENTER)
-root.protocol("WN_DELETE_WINDOW", root.destroy)
+
 root.mainloop()
