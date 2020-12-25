581c198ac536b21271efa5d58e4a3795614369f5
SantoshJonnakuti <santoshjonnakuti@gmail.com>
2020-12-25 14:13:05 +0530

    Update Main.py

--- Currency-Converter/Main.py
+++ Currency-Converter/Main.py
@@ -6,7 +6,12 @@ from bs4 import BeautifulSoup
 
 
 def Function(url):
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except Exception:
+        errorLabel.config(text="No Internet Connection...")
+        errorLabel.place(x=300, y=300, anchor=tk.CENTER)
+        return
     soup = BeautifulSoup(response.content, "html.parser")
     tags = soup.find("div", attrs={"class": "BNeawe iBp4i AP7Wnd"})
     tag = tags.find("div", attrs={"class": "BNeawe iBp4i AP7Wnd"})
@@ -53,7 +58,6 @@ root.geometry("600x500")
 root.resizable(0, 0)
 bgImage = ImageTk.PhotoImage(Image.open("Background.jpg"))
 bgLabel = tk.Label(root, image=bgImage).pack()
-
 fromLabel = tk.Label(root, text="From", bg="black", fg="white", font=("Helvetica", 12, "bold")).place(x=53, y=75, anchor=tk.CENTER)
 comboBox = ttk.Combobox(root, font=("Helvetica", 8))
 comboBox["values"] = ("Indian Rupee", "US Dollar", "British Pound", "Australian Dollar", "Canadian Dollar", "Japanese Yen", "Swiss Franc")
@@ -61,7 +65,6 @@ comboBox["state"] = "readonly"
 comboBox.place(x=100, y=100, anchor=tk.CENTER)
 entry = tk.Entry(root, width=15, bg="white", fg="black", font=("Helvetica", 10))
 entry.place(x=83, y=125, anchor=tk.CENTER)
-
 toLabel = tk.Label(root, text="To", bg="black", fg="white", font=("Helvetica", 12, "bold")).place(x=243, y=75, anchor=tk.CENTER)
 comboBox1 = ttk.Combobox(root, font=("Helvetica", 8))
 comboBox1["values"] = ("Indian Rupee", "US Dollar", "British Pound", "Australian Dollar", "Canadian Dollar", "Japanese Yen", "Swiss Franc")
@@ -69,9 +72,7 @@ comboBox1["state"] = "readonly"
 comboBox1.place(x=300, y=100, anchor=tk.CENTER)
 entry1 = tk.Entry(root, width=15, bg="white", fg="black", font=("Helvetica", 10))
 entry1.place(x=283, y=125, anchor=tk.CENTER)
-
 convertButton = tk.Button(root, text="Convert", bg="black", fg="white", font=("Helvetica", 12, "bold"), command=urlGenerator)
 convertButton.place(x=450, y=113, anchor=tk.CENTER)
 errorLabel = tk.Label(root, text="", bg="black", fg="red", font=("Helvetica", 12))
-
 root.mainloop()
