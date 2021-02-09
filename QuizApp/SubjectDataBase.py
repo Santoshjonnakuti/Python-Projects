35e993b1c75ee68192b2a0136cd852d4c76fc5cb
SantoshJonnakuti <santoshjonnakuti@gmail.com>
2021-02-09 19:38:08 +0530

    Update SubjectDataBase.py

--- QuizApp/SubjectDataBase.py
+++ QuizApp/SubjectDataBase.py
@@ -39,3 +39,26 @@ def showAllQuestion(subject):
         string = string + "Question : " + question[0] + "\nOption1 : " + question[1] + "\nOption2 : " + question[2] + \
                  "\nOption3 : " + question[3] + "\nOption4 : " + question[4] + "\nAnswer : " + question[5] + "\n\n"
     return string
+
+
+def getAllSubjects():
+    conn = sqlite3.connect("Subjects.sqlite")
+    cur = conn.cursor()
+    cur.execute('''SELECT name FROM sqlite_master WHERE type="table"''')
+    subjects = cur.fetchall()
+    string = ""
+    for subject in subjects:
+        string = string + subject[0] + "\n"
+    returnTuple = tuple(string.split())
+    return returnTuple
+
+
+def getAllQuestion(subject):
+    conn = sqlite3.connect("Subjects.sqlite")
+    cur = conn.cursor()
+    cur.execute('''SELECT * FROM {}'''.format(subject))
+    questions = cur.fetchall()
+    questionList = []
+    for question in questions:
+        questionList.append([question[0], question[1], question[2], question[3], question[4], question[5]])
+    return questionList
